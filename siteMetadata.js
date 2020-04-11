50e9ca56d507e73ed698696d2fdc2b9d61899118
kiluhabe <kiluhabe@users.noreply.github.com>
2020-04-11 15:56:21 +0900

    :wrench: fix siteUrl

--- siteMetadata.js
+++ siteMetadata.js
@@ -2,5 +2,5 @@ module.exports = {
     title: `Gatsby Heat`,
     description: `theme for gatsby`,
     author: `@kiluhabe`,
-    siteUrl: 'https://example.com',
+    siteUrl: 'https://gatsby-heat.now.sh',
 }
